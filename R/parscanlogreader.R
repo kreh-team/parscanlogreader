35380ea5638db7fe863860fecb7e5ac06c182d0a
Alfredo Hernández <aldomann.designs@gmail.com>
2020-01-02 15:46:55 +0000

    Added package-level documentation

---
+++ R/parscanlogreader.R
@@ -0,0 +1,11 @@
+#' \code{parscanlogreader} package
+#'
+#' See the README on
+#' \href{https://github.com/kreh-team/parscanlogreader}{GitHub}
+#'
+#' @docType package
+#' @name parscanlogreader
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
