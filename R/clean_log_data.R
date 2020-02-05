2708fa20d88f1fcd9507399655f9bd6248f3d251
Alfredo Hernández <aldomann.designs@gmail.com>
2020-02-05 15:15:45 +0000

    Using mutate_at for parsing relevant column info and added new drop_na parameter

--- R/clean_log_data.R
+++ R/clean_log_data.R
@@ -1,30 +1,43 @@
 #' Clean raw log data frame
 #'
 #' @param data Raw log data frame
+#' @param drop_na Drop rows containing missing values
 #'
 #' @return Clean log data frame
 #' @export
 #'
 #' @importFrom rlang .data
-clean_log_data <- function(data) {
+clean_log_data <- function(data, drop_na = FALSE) {
+  # Read search settings
+  search_settings <- attr(data, "search_settings")
+
+  # Drop NA columns
+  if (drop_na) {
+    data <- data %>%
+      tidyr::drop_na()
+  }
 
   # Use regex for getting the relevant content of each raw column
   data <- data %>%
-    dplyr::mutate(
-      epoch = stringr::str_extract(.data$epoch, "[0-9]*/[0-9]*"),
-      step = stringr::str_extract(.data$step, "[0-9]*/[0-9]*"),
-      loss = stringr::str_extract(.data$loss, "[0-9]*\\.[0-9]*"),
-      accuracy = stringr::str_extract(.data$accuracy, "[0-9]*\\.[0-9]*")
+    dplyr::mutate_at(
+      .vars = dplyr::vars(.data$epoch, .data$step),
+      .funs = function(x) stringr::str_extract(x, "[0-9]*/[0-9]*")
+    ) %>%
+    dplyr::mutate_at(
+      .vars = dplyr::vars(.data$loss, .data$accuracy),
+      .funs = function(x) stringr::str_extract(x, "[0-9]*\\.[0-9]*")
     )
 
-  # Change data types and remove useless column
+  # Change data types and remove useless columns
   data <- data %>%
-    dplyr::mutate(
-      run = as.numeric(.data$run),
-      loss = as.numeric(.data$loss),
-      accuracy = as.numeric(.data$accuracy)
+    dplyr::mutate_at(
+      .vars = dplyr::vars(.data$run, .data$loss, .data$accuracy),
+      .funs = as.numeric
     ) %>%
-    dplyr::select(-.data$eta, -.data$epochs_raw)
+    dplyr::select(-.data$eta, -.data$epochs)
+
+  # Set search settings attribute
+  attr(data, "search_settings") <- search_settings
 
   return(data)
 }
